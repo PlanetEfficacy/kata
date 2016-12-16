71b3a8e9345ac861c67388f6595aa54b3b18561d
PlanetEfficacy <jspevack@gmail.com>
2016-12-15 21:24:18 -0700

    Add duration to job

--- lib/job.rb
+++ lib/job.rb
@@ -1,2 +1,31 @@
 class Job
+  attr_reader :services,
+              :jobs
+
+  def initialize(*services)
+    @services = services
+    @jobs = {
+      wax: {
+        duration: 900
+      },
+      edge: {
+        duration: 1500
+      },
+      performance: {
+        duration: 4320
+      },
+      basic: {
+        # duration:
+      },
+      ptex: {
+        # duration:
+      }
+    }
+  end
+
+  def duration
+    services.reduce(0) do |sum, service|
+      sum += jobs[service][:duration]
+    end
+  end
 end
--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -1,8 +1,16 @@
 require_relative "../lib/job"
 
 describe Job, "#duration" do
-  xit "returns the total number of seconds the job will take to complete" do
-    pending
+  it "returns the total number of seconds the job will take to complete" do
+    job_1 = Job.new(:wax)
+    job_2 = Job.new(:edge, :wax)
+    job_3 = Job.new(:performance)
+    job_4 = Job.new(:basic, :ptex)
+
+    expect(job_1.duration).to eq(900)
+    expect(job_2.duration).to eq(2400)
+    expect(job_3.duration).to eq(4320)
+    # expect(job_4.duration).to eq(3600)
   end
 end
 
@@ -11,3 +19,20 @@ describe Job, "#price" do
     pending
   end
 end
+
+
+# job = Job.new(:wax)          #=> single service
+# job.duration                 #=> 900 (seconds)
+# job.price                    #=> 20.00 (dollars)
+#
+# job = Job.new(:edge, :wax)   #=> multiple services
+# job.duration                 #=> 2400 (seconds)
+# job.price                    #=> 53.33 (dollars)
+#
+# job = Job.new(:performance)  #=> a package
+# job.duration                 #=> 4320 (seconds)
+# job.price                    #=> 96.00 (dollars)
+#
+# job = Job.new(:basic, :ptex) #=> package + additional service
+# job.duration                 #=> 3600 (seconds)
+# job.price                    #=> 80.00 (dollars)
